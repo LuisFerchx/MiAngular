5928a7ba4a85baca86bdd151e0be39aeb2ac866b
LuisFerchx <luixg22@gmail.com>
2021-04-06 18:18:58 -0500

    primer commit

---
+++ ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'chat-app',
+    templateUrl: "./chat.component.html"
+})
+
+
+export class ChatComponet {
+    public nombre = "El huevazo";
+}
